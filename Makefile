ffa714c0a854992c37ed20dd94efba5f9e0a7b2d
Alexandre Fabry <alexfabry98@gmail.com>
2023-01-01 23:39:53 +0100

    first step for the api

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ clean:
 	@rm -fr */*/.ipynb_checkpoints
 	@rm -fr */*/*/.ipynb_checkpoints
 	@rm -fr *.egg-info
+
+
+run_api:
+	uvicorn api.fast:app --reload
+
+
--- api/fast.py
+++ api/fast.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from api.scrape import get_photo_text
+from api.scrape import scrape_text
 import pandas as pd
 from fastapi.middleware.cors import CORSMiddleware
+from keras.models import load_model
+from api.text_processing import preprocess_pad
 
 
-
+NLP_MODEL_PATH = 'saved_models/nlp/nlp_model.h5'
 app = FastAPI()
+app.state.model =load_model(NLP_MODEL_PATH)
 
 
 app.add_middleware(
@@ -16,8 +19,24 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
+#GET http://localhost:8000/predict?url='https://www.airgom-bruxelles.be/'
+
 @app.get("/predict")
-def predict(url):
-    text , image =get_photo_text(url)
-    x=pd.DataFrame({'text':text,
-                    'image':image})
+def predict(url:str):
+    X_text = scrape_text(url)
+
+    # Process text for model ingestion
+    X_text_padded = preprocess_pad([X_text])
+
+    # Do prediction with model
+    prediction = app.state.model.predict(X_text_padded)
+
+
+    return {'probability':float(prediction)}
+
+@app.get("/")
+def root():
+    return {
+    'greeting': 'Hello'
+}
